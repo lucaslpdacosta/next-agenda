57b37876abf0035a86d8acc4269723fad8de6234
Lucas Prudencio <lucaslpdacosta1@gmail.com>
2025-06-02 23:36:28 -0300

    feat: added clinic form page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
+
 import SignOutButton from "./components/sign-out-button";
 
 const DashboardPage = async () => {
@@ -9,6 +14,12 @@ const DashboardPage = async () => {
   });
   if (!session?.user) {
     redirect("/authentication");
+  }
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
   }
   return (
     <div>
