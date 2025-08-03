3c92267eec2dab3bb49e280b8fb4ba585f65020f
Lucas Prudencio <lucaslpdacosta1@gmail.com>
2025-08-02 21:22:58 -0300

    chore: insert redirect to dashboard page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /*
+  experimental: {
+    serverComponentsExternalPackages: ["better-auth"],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  */
 };
 
 export default nextConfig;
